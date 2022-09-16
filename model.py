c3c883d039e3fe66f68447eb100304e15abea4b7
higasun <higasun.lab@gmail.com>
2022-09-16 10:15:40 +0900

    modified

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 dx = [1, 1, 0, -1, -1, -1, 0, 1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
-GRID_LEN = 8
+GRID_LEN = 4
 DISK2COLOR = {'Dark': 1, 'Light': -1}
 
 class Player:
@@ -20,24 +20,34 @@ class Board:
         self.grid[GRID_LEN//2][GRID_LEN//2 - 1] = 1
 
     def place(self, x, y, color): # 返り値を「ひっくり返って増えた枚数」としても良いかも
-        if self.can_place(x, y, color):
-            self.grid[x][y] = color
-            self.reverse()
-            return True
-        else:
-            return False
+        dirs = self._get_dirs(x, y, color)
+
+        cnt = 0
+        for dir in dirs:
+            cnt += self._reverse(x, y, color, dir)
+        return cnt
+
 
     def can_place(self, x: int, y: int, color: int) -> bool:
         if not(0 <= x < GRID_LEN) or not (0 <= y < GRID_LEN) \
             or self.grid[x][y] != 0:
             return False
         
-        res = False
         for dir in range(8):
             if self._dfs_check(x, y, dir, color):
                 return True
         return False
 
+    def _get_dirs(self, x: int, y: int, color: int) -> list:
+        if not(0 <= x < GRID_LEN) or not (0 <= y < GRID_LEN) \
+            or self.grid[x][y] != 0:
+            return False
+        
+        dirs = []
+        for dir in range(8):
+            if self._dfs_check(x, y, dir, color):
+                dirs.append(dir)
+        return dirs
         
     def _dfs_check(self, x: int, y: int, dir: int, color: int, cnt=0) -> bool:
         if self.grid[x][y] != 0 and self.grid[x][y] != color:
@@ -52,8 +62,17 @@ class Board:
                 return True
         return False
 
-    def reverse(self, x, y, color):
-        pass
+    def _reverse(self, x, y, color, dir) -> int:
+        cnt = 0
+        self.grid[x][y] = color
+        x, y = x + dx[dir], y + dy[dir]
+        
+        while self.grid[x][y] == color * (-1):
+            self.grid[x][y] = color
+            cnt += 1
+            x, y = x + dx[dir], y + dy[dir]
+        
+        return cnt
 
 
 class Game:
@@ -64,28 +83,29 @@ class Game:
 
         self.player = self.dark_player
         self.next_player = self.light_player
-
-    def play(self):
-        self.show()
-        while True:
-            x, y = map(int, input("{}'s Turn: ".format(game.player.disk)).split())
-            self.place(x, y)
-            self.show()
     
     def place(self, x, y) -> None:
-        if self.board.place(x, y, self.player.color):
+        if self.board.can_place(x, y, self.player.color):
+            cnt = self.board.place(x, y, self.player.color)
+            self.player.count += cnt
+
+            if self._check():
+                self.end()
             self.player, self.next_player = self.next_player, self.player
         else:
             print('You cannot place a disk there.')
             
-    def show(self):
+    def show(self) -> None:
         label = {-1: 'o', 0: '-', 1: 'x'}
         for i in range(GRID_LEN):
             l = [label[x] for x in self.board.grid[i]]
             print(''.join(l))
         print('\n')
 
-    def end(self):
+    def _check(self) -> bool:
+        return self.dark_player.count + self.light_player.count >= GRID_LEN**2
+
+    def _end(self):
         if self.dark_player.count > self.light_player.count:
             return 'Dark WIN ---- Light LOSE'
         elif self.dark_player.count < self.light_player.count:
@@ -94,4 +114,8 @@ class Game:
             return 'DRAW'
 
 game = Game()
-game.play()
+game.show()
+while True:
+    x, y = map(int, input("{}'s Turn: ".format(game.player.disk)).split())
+    game.place(x, y)
+    game.show()
